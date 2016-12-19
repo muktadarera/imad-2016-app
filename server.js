526221b91f91f0fd03fa3767fc5db3a9b467842e
muktadarera <muktasmailbox@gmail.com>
2016-12-19 21:30:39 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function(req,res) {
+app.get('/article-one.html', function(req,res) {
     res.send('Article one');
 });
 
@@ -26,3 +26,4 @@ var port = 8080; // Use 8080 for local development because you might already hav
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
+
